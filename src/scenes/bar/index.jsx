0cc8973fdcf3a32a65f01f42b3dc3ecf6ea8d828
Aniket Bhangale <aniketbhangale10@gmail.com>
2024-09-07 00:46:58 +0530

    Update index.jsx

--- src/scenes/bar/index.jsx
+++ src/scenes/bar/index.jsx
@@ -1,6 +1,7 @@
 import { Box } from "@mui/material";
 import Header from "../../components/Header";
 import BarChart from "../../components/BarChart";
+
 const Bar = () => {
   return (
     <Box m="20px">
